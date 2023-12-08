740309fff64d11b7206fc7902b5d065a0c7f9c41
James Lewis <24jameslewis@usd266.com>
2023-12-08 10:23:38 -0600

    /

--- 08WhileLoopsForUserInput/Assignment/main.py
+++ 08WhileLoopsForUserInput/Assignment/main.py
@@ -1,18 +1,15 @@
 import random
-number = random.randint(1, 10)
+secret_number = random.randint(1, 10)
 def number_guesser():
-
     number_guess = ""
     invalid_guess = True
     while invalid_guess == True:
-        print("Guess a number from 1 to 10")
+        print("Guess a number from 1 - 10")
         number_guess = input()
-        if number_guess > number:
-            print("Your guess was too high")
-        elif number_guess < number:
-            print("Your guess was too low")
-
-        if number_guess is number:
-            invalid_guess == False
-print(random.randint(1, 10))
+        if number_guess == secret_number:
+            invalid_guess == False and print("You guessed correctly")
+        elif number_guess != secret_number:
+            invalid_guess == True and print("Try again!")
+            
+print(secret_number)
 number_guesser()
