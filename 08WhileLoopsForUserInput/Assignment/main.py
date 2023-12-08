29643410485dad3822ac00586ecc27c829f7883d
James Lewis <24jameslewis@usd266.com>
2023-12-08 09:52:02 -0600

    .

--- 08WhileLoopsForUserInput/Assignment/main.py
+++ 08WhileLoopsForUserInput/Assignment/main.py
@@ -1,1 +1,18 @@
 import random
+number = random.randint(1, 10)
+def number_guesser():
+
+    number_guess = ""
+    invalid_guess = True
+    while invalid_guess == True:
+        print("Guess a number from 1 to 10")
+        number_guess = input()
+        if number_guess > number:
+            print("Your guess was too high")
+        elif number_guess < number:
+            print("Your guess was too low")
+
+        if number_guess is number:
+            invalid_guess == False
+print(random.randint(1, 10))
+number_guesser()
