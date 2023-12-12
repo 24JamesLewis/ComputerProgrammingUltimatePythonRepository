ad4cfd39e2c24a98373a011065f119906eb085fb
James Lewis <24jameslewis@usd266.com>
2023-12-12 09:37:24 -0600

    random

--- 09WorkingWithFiles/Assignment/main.py
+++ 09WorkingWithFiles/Assignment/main.py
@@ -1,1 +1,14 @@
 import csv
+
+f = open("../data/4000-most-common-english-words.csv", "r")
+words = f.read().split("\n")
+f.close()
+
+def most_vowels(words):
+    total = 0
+    for letter in words:
+        if letter in "aeiou":
+            total = total + 1
+    return total
+
+print(most_vowels(words))
